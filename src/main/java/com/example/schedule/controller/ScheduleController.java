50b3e89bae046ddd56a29317a238edcd1439f83d
sosugyeong <sugyeong247@gmail.com>
2026-02-03 17:26:33 +0900

    Schedule Controller 생성

---
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -0,0 +1,12 @@
+package com.example.schedule.controller;
+
+import com.example.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+}
