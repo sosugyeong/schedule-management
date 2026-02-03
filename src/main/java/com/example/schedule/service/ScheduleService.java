4c46661867a38821beca036ea61e8d60034694eb
sosugyeong <sugyeong247@gmail.com>
2026-02-03 17:27:03 +0900

    Schedule Service 생성

---
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -0,0 +1,12 @@
+package com.example.schedule.service;
+
+import com.example.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+}
