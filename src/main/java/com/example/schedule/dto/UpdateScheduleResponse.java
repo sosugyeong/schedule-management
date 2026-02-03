d0cd541b1fd66eb975db121b189fa7967fc0b945
sosugyeong <sugyeong247@gmail.com>
2026-02-04 00:17:11 +0900

    선택 일정 수정 dto - 생성자 추가

--- src/main/java/com/example/schedule/dto/UpdateScheduleResponse.java
+++ src/main/java/com/example/schedule/dto/UpdateScheduleResponse.java
@@ -2,11 +2,21 @@ package com.example.schedule.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class UpdateScheduleResponse {
     private final Long id;
+    private final String title;
+    private final String content;
+    private final String userName;
+    private final LocalDateTime modifiedAt;
 
-    public UpdateScheduleResponse(Long id) {
+    public UpdateScheduleResponse(Long id, String title, String content, String userName, LocalDateTime modifiedAt) {
         this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userName = userName;
+        this.modifiedAt = modifiedAt;
     }
 }
