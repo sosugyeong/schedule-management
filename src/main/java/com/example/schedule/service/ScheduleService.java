e9e8cfae282593e7f898b0724406e6aad8d07d81
sosugyeong <sugyeong247@gmail.com>
2026-02-03 19:38:01 +0900

    @Transactional 안써줘서 추가

--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -2,10 +2,14 @@ package com.example.schedule.service;
 
 import com.example.schedule.dto.CreateScheduleRequest;
 import com.example.schedule.dto.CreateScheduleResponse;
+import com.example.schedule.dto.GetScheduleResponse;
 import com.example.schedule.entity.Schedule;
 import com.example.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +18,11 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
     //저장 (일정 생성)
+    @Transactional
     public CreateScheduleResponse save(CreateScheduleRequest request){
         Schedule schedule = new Schedule(
                 request.getTitle(),
-                request.getComment(),
+                request.getContent(),
                 request.getUserName(),
                 request.getPassword()
         );
@@ -25,7 +30,7 @@ public class ScheduleService {
         Schedule saveSchedule = scheduleRepository.save(schedule);
         return new CreateScheduleResponse(
                 saveSchedule.getTitle(),
-                saveSchedule.getComment(),
+                saveSchedule.getContent(),
                 saveSchedule.getUserName(),
                 saveSchedule.getCreatedAt(),
                 saveSchedule.getModifiedAt()
@@ -33,6 +38,9 @@ public class ScheduleService {
     }
 
     //전체 일정 조회
+    //public List<GetScheduleResponse> getAll() {
+        //List<>
+    //}
     //선택 일정 조회
     //선택 일정 수정
     //선택 일정 삭제
