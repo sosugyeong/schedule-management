4d7084ac1be25d3b955931b942bcb27aec84212d
sosugyeong <sugyeong247@gmail.com>
2026-02-04 20:23:17 +0900

    Lv5. 댓글 10개 제한 Lv6. 댓글 리스트에서 scheduleId 찾기

--- src/main/java/com/example/schedule/repository/CommentRepository.java
+++ src/main/java/com/example/schedule/repository/CommentRepository.java
@@ -3,5 +3,12 @@ package com.example.schedule.repository;
 import com.example.schedule.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByScheduleId(Long scheduleId);
+
+    //댓글 10개 제한
+    long countByScheduleId(Long scheduleId);
 }
