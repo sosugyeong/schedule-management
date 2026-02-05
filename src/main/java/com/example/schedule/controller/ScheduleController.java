396080ce60cdec46b38c83e674ced4757de9e7c2
sosugyeong <sugyeong247@gmail.com>
2026-02-05 19:28:17 +0900

    쿼리 파라미터로 변경

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -23,8 +23,8 @@ public class ScheduleController {
     }
 
     //전체 일정 조회
-    @GetMapping("/schedules/search/{userName}")
-    public ResponseEntity<List<GetScheduleResponse>> getSchedule(@PathVariable String userName){
+    @GetMapping("/schedules")
+    public ResponseEntity<List<GetScheduleResponse>> getSchedule(@RequestParam(name="userName") String userName){
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findAll(userName));
     }
 
